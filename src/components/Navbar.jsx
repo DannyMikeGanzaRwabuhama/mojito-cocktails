b735dc954ee3e525f8d6df4e35007ef426643e65
mike <unknownentity5648@gmail.com>
2025-07-16 19:39:08 +0200

    Project Completion - Add Contact component with GSAP animations and footer details.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
     return (
         <nav>
             <div>
-                <a href={"#home"} className={"flex items-center gap-2"}>
+                <a href={"#hero"} className={"flex items-center gap-2"}>
                     <img src="/assets/images/logo.png" alt="Logo"/>
                     <p>Velvet Pour</p>
                 </a>
